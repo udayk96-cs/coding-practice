cb6c04a5b8b6c011f22ebb4f900940b48e3c4f43
udayk96-cs <udaykunwar96@gmail.com>
2026-07-18 13:38:58 +0530

    fibonacci prgram

---
+++ fibonacci.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+
+public class fibonacci {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number to print fibonacci series - ");
+        int n= scanner.nextInt();
+        int n0 =0, n1=1;
+        for(int i = 0;i<n-1;i++){
+            System.out.print(n0 + " ");
+            int n2 = n1 + n0;
+            n0=n1;
+            n1=n2;
+        }
+    }
+}
+
